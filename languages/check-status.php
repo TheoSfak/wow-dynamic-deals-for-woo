c3851ca3127deb212460a8e08950d8bcbddacfce
Theodore Sfakianakis <theodore.sfakianakis@gmail.com>
2025-12-22 12:02:12 +0200

    Remove Greek translations - keep only POT file for manual translation via WordPress.org

--- languages/check-status.php
+++
@@ -1,6 +0,0 @@
-<?php
-$po = 'wow-dynamic-deals-for-woo-el.po';
-$content = file_get_contents($po);
-$count = preg_match_all('/msgstr ""$/m', $content);
-echo "Empty translations: $count\n";
-echo "File size: " . filesize($po) . " bytes\n";
